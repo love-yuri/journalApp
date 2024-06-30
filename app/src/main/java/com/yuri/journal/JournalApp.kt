6eede32fcf094aa826bbb8eb0b9d961c6ca3c1f0
love-yuri <yuri2078170658@gmail.com>
2024-06-30 22:12:19 +0800

    feat: 创建JournalApp类

---
+++ app/src/main/java/com/yuri/journal/JournalApp.kt
@@ -0,0 +1,11 @@
+package com.yuri.journal
+
+import android.app.Application
+import com.yuri.journal.utils.MessageUtils
+
+class JournalApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
