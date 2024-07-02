4c91b350537558af505dc54d2bccbd04a11a31f5
love-yuri <yuri2078170658@gmail.com>
2024-07-02 23:45:23 +0800

    feat: 添加 DataBaseConstant 常量类

---
+++ app/src/main/java/com/yuri/journal/constants/DataBaseConstant.kt
@@ -0,0 +1,10 @@
+package com.yuri.journal.constants
+
+object DataBaseConstant {
+
+    // 日记表的表名
+    const val JOURNAL_TABLE_NAME = "journal"
+
+    // 数据库存储在本地的文件名
+    const val DATABASE_FILE_NAME = "journal.db"
+}
