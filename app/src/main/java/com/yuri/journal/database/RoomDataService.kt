aaeac30cc836362cbebb6c4913b4c5269378d2c8
love-yuri <yuri2078170658@gmail.com>
2024-07-04 23:16:30 +0800

    feat(database): 添加了ListConverters到RoomDataService

--- app/src/main/java/com/yuri/journal/database/RoomDataService.kt
+++ app/src/main/java/com/yuri/journal/database/RoomDataService.kt
@@ -2,11 +2,15 @@ package com.yuri.journal.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.yuri.journal.database.converters.ListConverters
 import com.yuri.journal.database.dao.JournalDao
 import com.yuri.journal.database.entity.JournalEntity
 
 
+
 @Database(entities = [JournalEntity::class], version = 1)
+@TypeConverters(ListConverters::class)
 abstract class RoomDataService : RoomDatabase(){
     abstract fun journalDao(): JournalDao
 }
