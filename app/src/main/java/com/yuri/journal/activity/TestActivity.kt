3fb5b3d81326b961eb7446e5bd0d690ff2f70ffa
love-yuri <yuri2078170658@gmail.com>
2024-07-07 23:44:47 +0800

    feat: 新增 TestActivity 和相关布局

---
+++ app/src/main/java/com/yuri/journal/activity/TestActivity.kt
@@ -0,0 +1,14 @@
+package com.yuri.journal.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yuri.journal.R
+import com.yuri.journal.base.BaseActivity
+import com.yuri.journal.databinding.ActivityTestBinding
+
+class TestActivity : BaseActivity<ActivityTestBinding>() {
+
+}
