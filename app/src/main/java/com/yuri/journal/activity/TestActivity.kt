94e6a8d4cf1057629cbcb8d7eb706a3a66acdce6
love-yuri <yuri2078170658@gmail.com>
2024-07-09 23:41:17 +0800

    feat: 引入 AppBarLayout 组件

--- app/src/main/java/com/yuri/journal/activity/TestActivity.kt
+++ app/src/main/java/com/yuri/journal/activity/TestActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.AppBarLayout
 import com.yuri.journal.R
 import com.yuri.journal.base.BaseActivity
 import com.yuri.journal.databinding.ActivityTestBinding
