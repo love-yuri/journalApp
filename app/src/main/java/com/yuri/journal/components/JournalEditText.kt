9fd5dff4df840d73898ab4096330f51db10fe014
love-yuri <yuri2078170658@gmail.com>
2024-07-12 23:13:55 +0800

    feat: 新增 JournalEditText 组件

---
+++ app/src/main/java/com/yuri/journal/components/JournalEditText.kt
@@ -0,0 +1,13 @@
+package com.yuri.journal.components
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class JournalEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.editTextStyle
+) : AppCompatEditText(context, attrs, defStyleAttr) {
+
+}
