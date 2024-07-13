76f66dd3a34f706b4c6e1bef58d552204288b914
love-yuri <yuri2078170658@gmail.com>
2024-07-14 00:19:01 +0800

    feat: 初始化Activity生命周期回调

--- app/src/main/java/com/yuri/journal/JournalApp.kt
+++ app/src/main/java/com/yuri/journal/JournalApp.kt
@@ -1,14 +1,22 @@
 package com.yuri.journal
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.util.Log
+import com.yuri.journal.common.AppDestroyedCallBack
 import com.yuri.journal.database.AppDatabase
 import com.yuri.journal.utils.MessageUtils
 
 class JournalApp: Application() {
+
     override fun onCreate() {
         super.onCreate()
 
         // 初始化数据库
         AppDatabase.init(this)
+
+        // 初始化Activity周期函数
+        registerActivityLifecycleCallbacks(AppDestroyedCallBack)
     }
 }
