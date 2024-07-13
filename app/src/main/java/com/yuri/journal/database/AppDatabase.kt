771e15373d5226659f58757c0c51fb0a07445114
love-yuri <yuri2078170658@gmail.com>
2024-07-14 00:18:10 +0800

    feat(AppDatabase): 转移DAO

--- app/src/main/java/com/yuri/journal/database/AppDatabase.kt
+++ app/src/main/java/com/yuri/journal/database/AppDatabase.kt
@@ -1,12 +1,23 @@
 package com.yuri.journal.database
 
 import android.app.Application
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.yuri.journal.constants.DataBaseConstant
+import com.yuri.journal.database.converters.ListConverters
 import com.yuri.journal.database.dao.JournalDao
+import com.yuri.journal.database.entity.JournalEntity
 
 object AppDatabase: AutoCloseable {
+
+    @Database(entities = [JournalEntity::class], version = 1)
+    @TypeConverters(ListConverters::class)
+    abstract class RoomDataService : RoomDatabase(){
+        abstract fun journalDao(): JournalDao
+    }
+
     private lateinit var db: RoomDataService
     val journalDao: JournalDao get() = db.journalDao()
 
