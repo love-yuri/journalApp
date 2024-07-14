ebf0d0966bbd8438625a3e260c20ae6d0b7c7a03
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:41:57 +0800

    feat: 添加默认通知通道常量

--- app/src/main/java/com/yuri/journal/constants/ActivityConstant.kt
+++ app/src/main/java/com/yuri/journal/constants/ActivityConstant.kt
@@ -3,4 +3,10 @@ package com.yuri.journal.constants
 object ActivityConstant {
     // activity 跳转间传递配置文件的名称
     const val ACTIVITY_CONFIG_NAME = "config"
+
+    // 默认 通知通道id
+    const val DEFAULT_NOTIFY_CHANNEL_ID = "default"
+
+    // 默认 通知通道名称
+    const val DEFAULT_NOTIFY_CHANNEL_NAME = "default_name"
 }
