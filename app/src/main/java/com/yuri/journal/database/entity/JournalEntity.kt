49d35e4f5cc018c2d7816cb6ad8c651159553735
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:22 +0800

    feat(database): 更新JournalEntity表结构

--- app/src/main/java/com/yuri/journal/database/entity/JournalEntity.kt
+++ app/src/main/java/com/yuri/journal/database/entity/JournalEntity.kt
@@ -9,9 +9,9 @@ import com.yuri.journal.utils.TimeUtils
 @Entity(tableName = JOURNAL_TABLE_NAME)
 data class JournalEntity(
     @PrimaryKey(autoGenerate = true) val id: Int? = null, // 主键id
-    @ColumnInfo val title: String?, // 标题
+    @ColumnInfo val title: String? = null, // 标题
     @ColumnInfo val content: String, // 日记内容
-    @ColumnInfo val images: List<String>?, // 关联图片
+    @ColumnInfo val images: List<String>? = null, // 关联图片
     @ColumnInfo val createTime: String = TimeUtils.now, // 创建时间
     @ColumnInfo val updateTime: String = TimeUtils.now, // 更新时间
 )
