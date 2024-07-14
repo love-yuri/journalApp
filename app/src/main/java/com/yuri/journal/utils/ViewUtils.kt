b8dc28050cea0cc4030bf6d0d26ad181110352b0
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:42 +0800

    feat: 添加显示软键盘和检查通知权限的功能

--- app/src/main/java/com/yuri/journal/utils/ViewUtils.kt
+++ app/src/main/java/com/yuri/journal/utils/ViewUtils.kt
@@ -1,11 +1,17 @@
 package com.yuri.journal.utils
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.graphics.Rect
+import android.os.Build
+import android.provider.Settings
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity.INPUT_METHOD_SERVICE
+import androidx.core.app.NotificationManagerCompat
+
 
 object ViewUtils {
 
@@ -19,9 +25,35 @@ object ViewUtils {
         return toolbarRect.contains(x, y)
     }
 
+    /**
+     * 显示软键盘
+     */
     fun Activity.showSoftInput(view: View) {
         view.requestFocus()
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    /**
+     * 检查是否有通知权限
+     */
+    fun Activity.isOpenNotify(): Boolean {
+        try {
+            val from = NotificationManagerCompat.from(this)
+            return from.areNotificationsEnabled()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return false
+    }
+
+    /**
+     * 前往设置通知权限
+     */
+    fun Activity.goToSetNotify() {
+        startActivity(Intent().apply {
+            setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        })
     }
 }
