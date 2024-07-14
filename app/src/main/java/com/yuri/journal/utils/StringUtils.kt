28ca6b020d997d2ad66be24200bbbee48088cff2
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:37 +0800

    feat: 添加 StringUtils 工具类以处理字符串和 JSON 数据

---
+++ app/src/main/java/com/yuri/journal/utils/StringUtils.kt
@@ -0,0 +1,21 @@
+package com.yuri.journal.utils
+
+import android.text.TextUtils
+import android.widget.TextView
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.Json.Default.serializersModule
+import kotlinx.serialization.serializer
+
+object StringUtils {
+    inline fun <reified T> String.parseJson(): T {
+        return Json.decodeFromString(serializersModule.serializer(), this)
+    }
+
+    fun TextView.ellipsize(maxLength: Int) {
+        val originalText = text.toString()
+        if (originalText.length > maxLength) {
+            val ellipsizedText = TextUtils.ellipsize(originalText, paint, maxLength.toFloat(), TextUtils.TruncateAt.END)
+            text = ellipsizedText
+        }
+    }
+}
