fb6833f484e0025d335d0a70ffb0e9ef4dcf5097
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:48 +0800

    feat: 添加数据操作功能到JournalViewModel

--- app/src/main/java/com/yuri/journal/viewModel/JournalViewModel.kt
+++ app/src/main/java/com/yuri/journal/viewModel/JournalViewModel.kt
@@ -2,19 +2,72 @@ package com.yuri.journal.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.viewModelScope
 import com.yuri.journal.common.log
 import com.yuri.journal.database.AppDatabase
+import com.yuri.journal.database.dao.JournalDao
 import com.yuri.journal.database.entity.JournalEntity
 import kotlinx.coroutines.launch
 
 class JournalViewModel: ViewModel() {
-    val data: MutableLiveData<List<JournalEntity>> by lazy {
-        MutableLiveData<List<JournalEntity>>().also {
+    val data: MutableLiveData<MutableList<JournalEntity>> by lazy {
+        MutableLiveData<MutableList<JournalEntity>>().also {
+            log.i("初始化数据....")
             viewModelScope.launch {
+                it.value = AppDatabase.journalDao.list().toMutableList()
+            }
+        }
+    }
 
-                it.value = AppDatabase.journalDao.list()
-                log.i("初始化..., ${it.value}")
+    /**
+     * 插入数据
+     * 异步执行
+     */
+    fun insert(entity: JournalEntity) {
+        viewModelScope.launch {
+            val res = AppDatabase.journalDao.insert(entity)
+            if (res != 0L) {
+                data.value?.add(entity.copy(
+                    id = res.toInt()
+                ))
+                data.postValue(data.value)
+            }
+        }
+    }
+
+    /**
+     * 更新视图数据
+     */
+    fun update(entity: JournalEntity) {
+        if (entity.id == null) {
+            log.e("id为空，更新失败!!")
+            return
+        }
+        viewModelScope.launch {
+            val res = AppDatabase.journalDao.update(entity)
+            if (res != 0) {
+                val index = data.value?.indexOfFirst { it.id == entity.id }
+                if (index != null && index != -1) {
+                    data.value?.set(index, entity)
+                    data.postValue(data.value)
+                }
+            }
+        }
+    }
+
+    /**
+     * 删除数据
+     */
+    fun delete(id: Int) {
+        viewModelScope.launch {
+            val res = AppDatabase.journalDao.deleteById(id)
+            if (res != 0) {
+                val index = data.value?.indexOfFirst { it.id == id }
+                if (index != null && index != -1) {
+                    data.value?.removeAt(index)
+                    data.postValue(data.value)
+                }
             }
         }
     }
