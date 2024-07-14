057941e7c72c4a5d0c712c185156d2b04483d61f
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:41:24 +0800

    feat: 初始化全局周期数据

--- app/src/main/java/com/yuri/journal/JournalApp.kt
+++ app/src/main/java/com/yuri/journal/JournalApp.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.os.Bundle
 import android.util.Log
 import com.yuri.journal.common.AppDestroyedCallBack
+import com.yuri.journal.constants.GlobalSharedConstant
 import com.yuri.journal.database.AppDatabase
 import com.yuri.journal.utils.MessageUtils
 
@@ -18,5 +19,8 @@ class JournalApp: Application() {
 
         // 初始化Activity周期函数
         registerActivityLifecycleCallbacks(AppDestroyedCallBack)
+
+        //初始化全局周期数据
+        GlobalSharedConstant.init(this)
     }
 }
