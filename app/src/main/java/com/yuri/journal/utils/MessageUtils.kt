28f43924f50de255cc988991928af367decfc68b
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:32 +0800

    feat: 添加通知功能到消息工具类

--- app/src/main/java/com/yuri/journal/utils/MessageUtils.kt
+++ app/src/main/java/com/yuri/journal/utils/MessageUtils.kt
@@ -1,40 +1,78 @@
 package com.yuri.journal.utils
 
+import android.app.Activity
 import android.app.Application
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity.NOTIFICATION_SERVICE
+import androidx.core.app.NotificationCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.yuri.journal.R
+import com.yuri.journal.constants.ActivityConstant.DEFAULT_NOTIFY_CHANNEL_ID
+import com.yuri.journal.constants.ActivityConstant.DEFAULT_NOTIFY_CHANNEL_NAME
 
 /**
  * 全局消息提示工具
  * 上下文为activity
  */
-class MessageUtils(
-    private val context: Context
-) {
-    fun createToast(msg: String) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+object MessageUtils {
+    fun Context.createToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
-    fun createDialog(title: String, msg: String, listener: DialogInterface.OnClickListener? = null) {
-        MaterialAlertDialogBuilder(context)
+    fun Context.createDialog(title: String, msg: String, listener: DialogInterface.OnClickListener? = null) {
+        MaterialAlertDialogBuilder(this)
             .setTitle(title)
             .setMessage(msg)
-            .setNegativeButton(context.getString(R.string.ok), listener)
+            .setNegativeButton(getString(R.string.ok), listener)
             .show()
     }
 
-    fun createDialog(msg: String) {
-        createDialog(context.getString(R.string.error), msg)
+    fun Activity.createDialog(msg: String) {
+        createDialog(getString(R.string.error), msg)
     }
 
-    fun valueCheck(view: Any?, msg: String): Boolean {
-        if (view.toString().isEmpty()) {
-            createDialog(context.getString(R.string.valueCheckError), msg)
-            return false
+    /**
+     * 发送通知
+     */
+    fun Context.notify(msg: String, intent: Intent? = null) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        val intent = Intent(this, EditJournalActivity::class.java)
+        var pendingIntent: PendingIntent? = null
+        intent?.also {
+            pendingIntent = PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
         }
-        return true
+        val mChannel = NotificationChannel(DEFAULT_NOTIFY_CHANNEL_ID, DEFAULT_NOTIFY_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+        mChannel.setShowBadge(true)
+        mChannel.enableLights(true)
+
+        notificationManager.createNotificationChannel(mChannel)
+
+        val notification = NotificationCompat.Builder(this, "default")
+            .setContentTitle("标题") //设置通知栏标题
+            .setContentText(msg) //设置通知栏显示内容
+            .setWhen(System.currentTimeMillis()) //通知产生的时间。
+            .setSmallIcon(android.R.mipmap.sym_def_app_icon) //设置通知小ICON
+            .setDefaults(NotificationManager.IMPORTANCE_HIGH)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .setAutoCancel(true) // 自动关闭
+            .setDefaults(Notification.DEFAULT_ALL) //.setFullScreenIntent(pendingIntent, true)
+            .setContentIntent(pendingIntent)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    android.R.mipmap.sym_def_app_icon
+                )
+            ) //设置通知大ICON
+            .build()
+
+        notificationManager.notify(1, notification)
     }
 }
