e24be0e3a9848e57ecfb66062b7d98c7a9bd389f
love-yuri <yuri2078170658@gmail.com>
2024-07-14 23:42:18 +0800

    feat(database): 更新JournalDao接口，增加返回值类型

--- app/src/main/java/com/yuri/journal/database/dao/JournalDao.kt
+++ app/src/main/java/com/yuri/journal/database/dao/JournalDao.kt
@@ -13,13 +13,13 @@ import com.yuri.journal.database.entity.JournalEntity
 interface JournalDao {
 
     @Insert
-    suspend fun insert(value: JournalEntity)
+    suspend fun insert(value: JournalEntity): Long
 
     @Update
-    suspend fun update(value: JournalEntity)
+    suspend fun update(value: JournalEntity): Int
 
     @Delete
-    suspend fun delete(value: JournalEntity)
+    suspend fun delete(value: JournalEntity): Int
 
     @Query("select * from $JOURNAL_TABLE_NAME")
     suspend fun list(): List<JournalEntity>
@@ -28,5 +28,5 @@ interface JournalDao {
     suspend fun deleteById(id: Int): Int
 
     @Query("select * from $JOURNAL_TABLE_NAME where id = :id")
-    suspend fun getById(id: Int): JournalEntity
+    suspend fun getById(id: Int): JournalEntity?
 }
