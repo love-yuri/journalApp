320a5cb372fdc956cc6ce6a568613d9c2af69bf6
love-yuri <yuri2078170658@gmail.com>
2024-07-15 23:43:07 +0800

    feat(JournalEditText): 添加实时文本监听功能

--- app/src/main/java/com/yuri/journal/components/JournalEditText.kt
+++ app/src/main/java/com/yuri/journal/components/JournalEditText.kt
@@ -1,6 +1,8 @@
 package com.yuri.journal.components
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 
@@ -9,5 +11,23 @@ class JournalEditText @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = android.R.attr.editTextStyle
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
+    var changeCallBack: ((text: String) -> Unit)? = null
 
+    init {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                changeCallBack?.also {
+                    it(s.toString())
+                }
+            }
+        })
+    }
 }
