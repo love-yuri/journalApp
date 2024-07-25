d6096b28045af77eb7b5e757da62665f0e48e24d
love-yuri <yuri2078170658@gmail.com>
2024-07-26 00:18:45 +0800

    feat: 移除 WebDavConfig 中的用户名和密码常量

--- app/src/main/java/com/yuri/journal/constants/WebDavConfig.kt
+++ app/src/main/java/com/yuri/journal/constants/WebDavConfig.kt
@@ -2,7 +2,5 @@ package com.yuri.journal.constants
 
 object WebDavConfig {
     const val HOST = "https://dav.jianguoyun.com/"
-    const val username = "2078170658@qq.com"
-    const val password = "anp5yqxr435upzhu"
     const val DB_FOLDER = "journal" // 数据库上传的文件夹名称
 }
